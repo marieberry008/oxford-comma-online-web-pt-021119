40d9f0f25da45c1967ec298ef6de780548fbe6a7
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-11 00:35:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,13 @@
 array = ["fiddleheads"]
+
 def oxford_comma(array)
   if array.length == 2
   array[-2] << " and "
   array.join
   elsif array.length == 1
-  	array.join
+  array.join
   elsif array.length > 2
-    array[-1].prepend "and "
+  array[-1].prepend "and "
 	array.join(", ")
   end
 end
